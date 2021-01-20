db8ad00f619978cf802e2b0408ac0498013484b4
laurynasg <glaurynasg@gmail.com>
2021-01-20 16:49:04 +0200

    Add FAQ accordion

---
+++ src/containers/faq.js
@@ -0,0 +1,16 @@
+import faqsDate from '../fixtures/faqs.json';
+import { Accordion } from '../components';
+
+export function FaqsContainer() {
+    return (
+        <Accordion>
+            <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+            {faqsDate.map(item =>
+                <Accordion.Item>
+                    <Accordion.Header>{item.header}</Accordion.Header>
+                    <Accordion.Body>{item.body}</Accordion.Body>
+                </Accordion.Item>
+            )}
+        </Accordion>
+    )
+}
