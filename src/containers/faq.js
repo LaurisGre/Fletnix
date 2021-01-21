37ef59d1850880e6a936513a44e34f74af141142
laurynasg <glaurynasg@gmail.com>
2021-01-21 14:14:04 +0200

    Add opt-form component

--- src/containers/faq.js
+++ src/containers/faq.js
@@ -1,16 +1,24 @@
 import faqsDate from '../fixtures/faqs.json';
 import { Accordion } from '../components';
+import OptForm from '../components/opt-form';
 
 export function FaqsContainer() {
     return (
         <Accordion>
             <Accordion.Title>Frequently Asked Questions</Accordion.Title>
             {faqsDate.map(item =>
-                <Accordion.Item>
+                <Accordion.Item key={item.id}>
                     <Accordion.Header>{item.header}</Accordion.Header>
                     <Accordion.Body>{item.body}</Accordion.Body>
                 </Accordion.Item>
             )}
+
+            <OptForm>
+                <OptForm.Input placeholder="Enter your email here" />
+                <OptForm.Button>Try it now!</OptForm.Button>
+                <OptForm.Text>Ready to watch? Get the best deal now!</OptForm.Text>
+            </OptForm>
+
         </Accordion>
     )
 }
