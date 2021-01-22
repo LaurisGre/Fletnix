4d6ffa2dc79dbb523307758e3fd2f1f5d1b9cf95
laurynasg <glaurynasg@gmail.com>
2021-01-22 13:56:17 +0200

    Edit signup page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -60,7 +60,7 @@ export default function Signin() {
                     </Form.Text>
                     <Form.TextSmall>
                         This page is protected by Google reCAPTCHA to ensure you're not a bot :)
-                </Form.TextSmall>
+                    </Form.TextSmall>
                 </Form>
             </HeaderContainer>
             <FooterContainer />
