3300928a4b0a1bb65ea7debcb5edbd6e39592016
laurynasg <glaurynasg@gmail.com>
2021-01-22 13:29:49 +0200

    Add form component: Edit signin page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,3 +1,69 @@
-export default function Signin({ }) {
-    return <p>Hai this is sign in</p>
+import { useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FirebaseContext } from '../context/firebase';
+import { HeaderContainer } from '../containers/header';
+import { FooterContainer } from '../containers/footer';
+import { Form } from '../components';
+import * as ROUTES from '../constants/routes';
+
+export default function Signin() {
+    const history = useHistory();
+    const { firebase } = useContext(FirebaseContext);
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const isInvalid = password === '' || emailAddress === '';
+
+    const handleSignin = (event) => {
+        event.preventDefault();
+
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(emailAddress, password)
+            .then(() => {
+                history.push(ROUTES.BROWSE);
+            })
+            .catch((error) => {
+                setEmailAddress('');
+                setPassword('');
+                setError(error.message);
+            });
+    };
+
+    return (
+        <>
+            <HeaderContainer>
+                <Form>
+                    <Form.Title>Sign In</Form.Title>
+                    {error && <Form.Error>{error}</Form.Error>}
+
+                    <Form.Base onSubmit={handleSignin} method="POST">
+                        <Form.Input
+                            placeholder="Email addresss"
+                            value={emailAddress}
+                            onChange={({ target }) => setEmailAddress(target.value)}
+                        />
+                        <Form.Input
+                            type="password"
+                            autoComplete="off"
+                            placeholder="Password"
+                            value={password}
+                            onChange={({ target }) => setPassword(target.value)}
+                        />
+                        <Form.Submit disabled={isInvalid} type="submit" >
+                            Sign In
+                    </Form.Submit>
+                    </Form.Base>
+                    <Form.Text>
+                        New to Fletnix? <Form.Link to="/signup">Sign up now!</Form.Link>
+                    </Form.Text>
+                    <Form.TextSmall>
+                        This page is protected by Google reCAPTCHA to ensure you're not a bot :)
+                </Form.TextSmall>
+                </Form>
+            </HeaderContainer>
+            <FooterContainer />
+        </>
+    )
 }
