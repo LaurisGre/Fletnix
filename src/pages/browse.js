593d3e65624feea7210eb199011614464256cd0e
laurynasg <glaurynasg@gmail.com>
2021-01-24 16:28:59 +0200

    Add useContent hook

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,3 +1,12 @@
+import { useContent } from '../hooks';
+
 export default function Browse({ }) {
+    // We need the series and the films
+    const { series } = useContent('series');
+    const { films } = useContent('films');
+
+    // we need slides
+    // pass it to the browse container
+
     return <p>Hai this is browse</p>
 }
